c4cdcc4c60ed5ab311d29e78fe518f13a3ab5258
Mori116 <cacaomame096@gmail.com>
2021-07-05 03:07:57 +0000

    [Add]ユーザ側マイページ、ユーザ情報編集画面の作成

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,10 +1,32 @@
 class Public::UsersController < ApplicationController
+
   def show
+    @user = User.find(params[:id])
+    # ここに通知の変数記述する
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render "edit"
+    end
   end
 
   def quit
+    @user = User.find(params[:id])
   end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :nickname, :email)
+  end
+
 end
