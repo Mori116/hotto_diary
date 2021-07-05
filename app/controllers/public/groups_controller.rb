2f7039237055be814e88a22a1b0e2e4d158be40c
Mori116 <cacaomame096@gmail.com>
2021-07-05 06:12:46 +0000

    [Add]ユーザ側グループ一覧、新規作成

--- app/controllers/public/groups_controller.rb
+++ app/controllers/public/groups_controller.rb
@@ -1,11 +1,26 @@
 class Public::GroupsController < ApplicationController
+
   def index
+    @groups = Group.all.order(created_at: :desc).page(params[:page]).per(10)
   end
 
   def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.owner_id = current_user.id
+    @group.users << current_user
+    if @group.save
+      redirect_to group_path(@group)
+    else
+      render "new"
+    end
   end
 
   def show
+    @group = Group.find(params[:id])
   end
 
   def edit
@@ -15,5 +30,12 @@ class Public::GroupsController < ApplicationController
   end
 
   def join
+  end
+
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :image, :password, :password_confirmation)
   end
 end
