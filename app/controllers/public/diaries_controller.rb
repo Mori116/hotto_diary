ec370e316205a709605bf89ca0d64850134c2121
Mori116 <cacaomame096@gmail.com>
2021-07-06 12:14:03 +0000

    [Add]ユーザ側日記一覧、新規作成

--- app/controllers/public/diaries_controller.rb
+++ app/controllers/public/diaries_controller.rb
@@ -7,12 +7,34 @@ class Public::DiariesController < ApplicationController
   end
 
   def new
+    @diary = Diary.new
+  end
+
+  def create
+    @group = Group.find(params[:group_id])
+    @diary = Diary.new(diary_params)
+    @diary.user_id = current_user.id
+    @diary.group_id = Group.find(params[:group_id]).id
+    if @diary.save
+      redirect_to group_diary_path(@group.id, @diary.id)
+    else
+      render "new"
+    end
   end
 
   def show
+    @group = Group.find(params[:group_id])
+    @diary = Diary.find_by(id: params[:id], group_id: @group)
   end
 
   def edit
+  end
+
+
+  private
+
+  def diary_params
+    params.require(:diary).permit(:title, :body, :image, :status)
   end
 
 end
