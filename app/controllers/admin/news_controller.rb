6676e37a714487f30036fb25206edfae39e53700
Mori116 <cacaomame096@gmail.com>
2021-07-07 05:31:01 +0000

    [Update]Top画面のお知らせ一覧、[Add]管理者側お知らせ

--- app/controllers/admin/news_controller.rb
+++ app/controllers/admin/news_controller.rb
@@ -1,7 +1,26 @@
 class Admin::NewsController < ApplicationController
+
   def new
+    @news = News.new
+  end
+
+  def create
+    @news = News.new(news_params)
+    if @news.save
+      redirect_to root_path
+    else
+      render "new"
+    end
   end
 
   def edit
   end
+
+
+  private
+
+  def news_params
+    params.require(:news).permit(:body)
+  end
+
 end
