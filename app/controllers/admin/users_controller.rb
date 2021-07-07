12ab64570ef4203c2b5894c00fdc197b3fb2b79e
Mori116 <cacaomame096@gmail.com>
2021-07-07 07:49:05 +0000

    [Add]管理者側ユーザ編集

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -15,6 +15,23 @@ class Admin::UsersController < ApplicationController
   # 所属グループの表示
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user.id)
+    else
+      render "edit"
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :nickname, :email, :is_deleted)
   end
 
 end
