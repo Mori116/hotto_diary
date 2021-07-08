d924b6e67cc2875fd8ecde7788ff47c67fe9d245
Mori116 <cacaomame096@gmail.com>
2021-07-08 07:12:19 +0000

    [Update]ルーティング整理,[Add]管理者側全日記一覧

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -4,6 +4,11 @@ class Admin::GroupsController < ApplicationController
     @groups = Group.all.order(created_at: :desc).page(params[:page]).per(10)
   end
 
+  def diaries
+    @diaries = Diary.all.order(created_at: :desc).page(params[:page]).per(10)
+  end
+  # 全日記一覧画面
+
   def show
     @group = Group.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,35 +5,35 @@ Rails.application.routes.draw do
     sessions: 'admins/sessions'
   }
 
-# ユーザ側
-scope module: :public do
-  root to: 'homes#top'
-  get 'homes/about' => "homes#about"
+  # ユーザ側
+  scope module: :public do
+    root to: 'homes#top'
+    get 'homes/about' => "homes#about"
 
-  resources :users, only: [:show, :edit, :update] do
-    member do
-    get 'quit'
-    patch 'withdraw'
-    get 'join_groups'
+    resources :users, only: [:show, :edit, :update] do
+      member do
+        get 'quit'
+        patch 'withdraw'
+        get 'join_groups'
+      end
     end
+
+    resources :groups do
+      resources :diaries do
+        resources :diary_comments, only: [:create, :destroy]
+      end
+      member do
+        get 'join'
+        post 'join_create'
+        delete 'exit'
+      end
+    end
+
+    get 'search' => "searches#search"
+
   end
 
-  resources :groups do
-    resources :diaries do
-      resources :diary_comments, only: [:create, :destroy]
-    end
-    member do
-      get 'join'
-      post 'join_create'
-      delete 'exit'
-    end
-  end
-
-  get 'search' => "searches#search"
-
-end
-
-# 管理者側
+  # 管理者側
   namespace :admin do
     resources :news, only: [:new, :create, :edit, :update, :destroy]
     resources :users, only: [:index, :show, :edit, :update] do
@@ -43,9 +43,12 @@ end
     end
 
     resources :groups, only: [:index, :show, :edit, :update, :destroy] do
-     resources :diaries, only: [:index, :show, :edit, :update, :destroy] do
-      resources :diary_comments, only: [:destroy]
-    end
+      collection do
+        get 'diaries'
+      end
+      resources :diaries, only: [:index, :show, :edit, :update, :destroy] do
+        resources :diary_comments, only: [:destroy]
+      end
     end
 
     get 'search' => "searches#search"
