b72b522d363411c2998c7ab7e81a8d08b7855514
Mori116 <cacaomame096@gmail.com>
2021-07-08 05:35:22 +0000

    [Add]管理者側グループ編集/削除

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -9,6 +9,28 @@ class Admin::GroupsController < ApplicationController
   end
 
   def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to admin_group_path(@group)
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to admin_groups_path
+  end
+
+  private
+
+  def group_params
+    params.requre(:group).permit(:name, :image)
   end
 
 end
