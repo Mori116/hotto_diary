2c65d78ce0af673530e3c4ea97fec190eb0c2e29
Mori116 <cacaomame096@gmail.com>
2021-07-12 12:21:56 +0000

    [Update]サインアップ/フッターのレイアウト修正、通知表示の修正

--- app/helpers/public/notifications_helper.rb
+++ app/helpers/public/notifications_helper.rb
@@ -3,13 +3,12 @@ module Public::NotificationsHelper
     def notification_form(notification)
       @visitor = notification.visitor
       @comment = nil
-      @your_diary = link_to 'あなたの投稿', group_diary_path(notification), style: "font-weight: bold;"
       @visiter_comment = notification.diary_comment_id
       #notification.actionがfollowかlikeかcommentか
       case notification.action
         when "comment" then
           @comment = DiaryComment.find_by(id: @visiter_comment)&.comment
-          tag.a(@visitor.nickname, style: "font-weight: bold;")+"が"+tag.a('あなたの投稿', href: group_diary_path(notification.diary_id), style: "font-weight: bold;")+"にコメントしました"
+          tag.a(@visitor.nickname, style: "font-weight: bold;")+"が"+tag.a('あなたの投稿', href: group_diary_path(notification.diary.group_id, notification.diary_id), style: "font-weight: bold;")+"にコメントしました"
       end
     end
 
