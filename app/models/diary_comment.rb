5d54e6beb862b1072c63850e9831abc7da62d7e9
Mori116 <cacaomame096@gmail.com>
2021-07-16 05:50:49 +0000

    [Update]日記作成者にのみ通知設定

--- app/models/diary_comment.rb
+++ app/models/diary_comment.rb
@@ -6,12 +6,9 @@ class DiaryComment < ApplicationRecord
   validates :comment, presence: true
 
   def create_notification_comment!(current_user, comment_id, diary_user_id)
-    user_ids = DiaryComment.where(diary_id: self.diary_id).where.not(user_id: current_user.id).pluck(:user_id)
-    user_ids << diary_user_id
-    # 自分以外にコメントしている人をすべて取得し、全員に通知を送る
-    user_ids.uniq.each do |user_id|
-      save_notification_comment!(current_user, self.id, user_id)
-    end
+    user_id = diary.user_id
+    save_notification_comment!(current_user, self.id, user_id)
+    # 日記作成者のみに通知する
   end
 
   def save_notification_comment!(current_user, diary_comment_id, visited_id)
@@ -22,6 +19,7 @@ class DiaryComment < ApplicationRecord
       action: 'comment'
     )
     # コメントは複数回することが考えられるため、１つの投稿に複数回通知する
+
     if notification.visitor_id == notification.visited_id
       notification.checked = true
     # 自分の投稿に自分がコメントした場合は、通知済みとする
